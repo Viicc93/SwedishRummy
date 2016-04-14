5fc238c924602ab49f8580f37236560bca4d4b27
Viicc93 <victoriajohansson93@hotmail.com>
2016-04-14 11:37:05 +0200

    & Jens added some HTML for game table

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8"/>
+	<link rel="stylesheet" href="css/style.css">
+	<title>Swedish Rummy</title>
+</head>
+
+<body>
+	<?php
 
 	spl_autoload_register( function($className)
 	{
@@ -6,3 +15,40 @@
 	});
 
  ?>
+
+
+<header><h1>Swedish Rummy</h1></header>
+
+<div id="wrap">
+
+<div id="table">
+
+<div class="player player1"></div>
+
+<div id="deck"></div>
+<div id="playedCard"></div>
+<div id="messageBox"></div>
+
+<div class="player player2"></div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+
+<footer></footer>
+
+</body>
+</html>
+
+
