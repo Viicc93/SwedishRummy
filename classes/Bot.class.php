81b85cc67249167dd1e734f6dac8abbc52944d1f
Viicc93 <victoriajohansson93@hotmail.com>
2016-04-16 10:24:24 +0200

    Created Bot and User Players classes

---
+++ classes/Bot.class.php
@@ -0,0 +1,20 @@
+<?php
+class Bot extends Player {
+
+	function __construct($name){
+		Parent::__construct($name);
+	}
+
+	public function PlayCard(){
+		# code for player to play a card on hand...
+	}
+	public function DrawCard(){
+		# code for player to draw a card when no match on hand....
+	}
+	public function RenderCard(){
+		# code to render cards on hand...
+	}
+
+}
+
+ ?>
