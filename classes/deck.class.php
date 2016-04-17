7ba9f74d60d73f32936f44db36f0ccb46ab9d956
Aydin Aydin <Aydin@Aydins-MacBook-Air.local>
2016-04-17 21:18:02 +0200

    uppdaterar mig

--- classes/deck.class.php
+++ classes/deck.class.php
@@ -1,45 +1,41 @@
 <?php
 class Deck {
-  /**
-   * Card array
-   *
-   * @var array
-   */
+	/**
+	 * Card array
+	 *
+	 * @var array
+	 */
 
 	private $_cards;
 
-  /**
-   * The constructor define $_cards array.
-   *
-   *
-   */
+	/**
+	 * The constructor define $_cards array.
+	 *
+	 *
+	 */
 
-  public function __construct()
-  {
-    $this->_cards = [];
-  }
+	public function __construct() {
+		$this->_cards = [];
+	}
 
-  ########################################################################
-  # PUBLIC METHODS                                                       #
-  ########################################################################
+	########################################################################
+	# PUBLIC METHODS                                                       #
+	########################################################################
 
-  /**
-   * Manipulate $_cards array with card object.
-   * This method uses composition to manipulate
-   * $_cards array with all card objects.
-   *
-   *  @param object  Card  A card object
-   */
+	/**
+	 * Manipulate $_cards array with card object.
+	 * This method uses composition to manipulate
+	 * $_cards array with all card objects.
+	 *
+	 *  @param object  Card  A card object
+	 */
 
-  public function setCards(Card $card)
-  {
-    // push card object to $_cards array
-    array_push($this->_cards, $card);
-  }
+	public function setCards(Card $card) {
+		// push card object to $_cards array
+		array_push($this->_cards, $card);
+	}
 
-
-  public function getCards()
-  {
-    return $this->_cards;
-  }
+	public function getCards() {
+		return $this->_cards;
+	}
 }
