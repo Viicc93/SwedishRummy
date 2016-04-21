8007ca53df04a1b6c534154e2d0347c3e2f54b7b
Aydin Aydin <Aydin@Aydins-MacBook-Air.local>
2016-04-21 10:00:00 +0200

    la till index och cards.json

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 require_once 'config/config.php';
 require_once 'inc/setup.php';
 
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
 error_reporting(-1);
 $allCards = [];
 
@@ -44,79 +44,76 @@ define('PATH_TO_CARDS_JSON_FILE', 'json/cards.json');
 						<!-- show messages -->
 						<div class="messages">
 							<!-- check if missing fields session has been started -->
-							<?php if(Session::getSession('missing')): ?>
-								<!-- assign missing array to $missing variable -->
-								<?php $missing = Session::flashSession('missing'); ?>
-								<!-- loop through $missing variable -->
-								<?php foreach($missing as $field): ?>
+<?php if (Session::getSession('missing')):?>
+<!-- assign missing array to $missing variable -->
+<?php $missing = Session::flashSession('missing');?>
+<!-- loop through $missing variable -->
+<?php foreach ($missing as $field):?>
 									<?php switch ($field) {
-										case 'user':
-										?>
-										<p>OBS: You forget to cheese a User Name!</p>
+	case 'user':
+		?>
+		<p>OBS: You forget to cheese a User Name!</p>
 
-										<?php break;} ?>
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div><!-- end messages -->
+		<?php break;}?>
+								<?php endforeach;?>
+							<?php endif;?>
+</div><!-- end messages -->
 						<h2>Bot user</h2>
-						<?php if(filter_has_var(INPUT_POST, 'submit')): ?>
-							<?php $username =   $_POST['user'];?>
-							<?php $gameUsers = $deck->getUser(); ?>
-							<?php foreach($gameUsers as $user): ?>
-								<?php if($user->name !== $username): ?>
+<?php if (filter_has_var(INPUT_POST, 'submit')):?>
+							<?php $username  = $_POST['user'];?>
+							<?php $gameUsers = $deck->getUser();?>
+							<?php foreach ($gameUsers as $user):?>
+								<?php if ($user->name !== $username):?>
 									<div class="bot-data">
-										<p><?php echo $user->name ?></p>
-											<?php
-											  //drfine json file url
-               $deck->dealTest();         //
-               print_r($bot->getCardsArray());
+										<p><?php echo $user->name?></p>
+<?php
+//drfine json file url
+//$deck->dealTest();         //
+//print_r($bot->getCardsArray());
 
-											  $allCards['botUser'] = ['bot' => $user->name, 'cards' => $user->getCardsArray()];
-											  $allCards['invisibleCardsOnTable'] = ['cardsOnTable' => $deck->getCardOnTable()];
-											  $allCards['visibleCardsOnTable'] = [];
-											  file_put_contents(PATH_TO_CARDS_JSON_FILE, json_encode($allCards, JSON_FORCE_OBJECT));
+$allCards['botUser']               = ['bot'          => $user->name, 'cards'          => $user->getCardsArray()];
+$allCards['invisibleCardsOnTable'] = ['cardsOnTable' => $deck->getCardOnTable()];
+$allCards['visibleCardsOnTable']   = [];
+file_put_contents(PATH_TO_CARDS_JSON_FILE, json_encode($allCards, JSON_FORCE_OBJECT));
 
-											?>
+?>
 									</div><!-- end bot-data -->
-									<div data-userId="<?php echo $user->getId(); ?>" class="cards-on-table-users bot-cards">
-										<?php  for ($i=0; $i < count($user->getCardsArray()); $i++): ?>
+									<div data-userId="<?php echo $user->getId();?>" class="cards-on-table-users bot-cards">
+<?php for ($i = 0; $i < count($user->getCardsArray()); $i++):?>
 											<div class="card-pos">
 
-											<a href="" data-id="<?php echo $user->getCardsArray()[$i]->getCardId(); ?>"><img src="<?php echo $user->getCardsArray()[$i]->getCardHref(); ?>" alt=""></a>
+											<a href="" data-id="<?php echo $user->getCardsArray()[$i]->getCardId();?>"><img src="<?php echo $user->getCardsArray()[$i]->getCardHref();?>" alt=""></a>
 											</div><!-- end card-pos -->
-										<?php endfor; ?>
-									</div><!-- end bot-cards -->
+<?php endfor;?>
+</div><!-- end bot-cards -->
 						</div><!-- end bot-user -->
-								<?php else: ?>
-									<div data-userId="<?php echo $user->getId(); ?>" class="cards-on-table-users user-cards">
-									<?php $allCards['humanUser'] = ['user1' => $username, 'cards' => $user->getCardsArray()] ?>
-									<?php  file_put_contents(PATH_TO_CARDS_JSON_FILE, json_encode($allCards, JSON_PRETTY_PRINT));?>
-									<h3>User Cards</h3>
+<?php  else :?>
+									<div data-userId="<?php echo $user->getId();?>" class="cards-on-table-users user-cards">
+<?php $allCards['humanUser'] = ['user1' => $username, 'cards' => $user->getCardsArray()]?>
+									<?php file_put_contents(PATH_TO_CARDS_JSON_FILE, json_encode($allCards, JSON_PRETTY_PRINT));?>
+<h3>User Cards</h3>
 
-										<?php  for ($i=0; $i < count($user->getCardsArray()); $i++): ?>
+<?php for ($i = 0; $i < count($user->getCardsArray()); $i++):?>
 											<div class="card-pos">
 
-											<a href="" data-id="<?php echo $user->getCardsArray()[$i]->getCardId(); ?>"><img src="<?php echo $user->getCardsArray()[$i]->getCardHref(); ?>" alt=""></a>
+											<a href="" data-id="<?php echo $user->getCardsArray()[$i]->getCardId();?>"><img src="<?php echo $user->getCardsArray()[$i]->getCardHref();?>" alt=""></a>
 											</div><!-- end card-pos -->
-										<?php endfor; ?>
+<?php endfor;?>
+</div><!-- end user-cards -->
+<?php endif;?>
+							<?php endforeach;?>
+						<?php endif;?>
+<div class="middle-row col-md-4">
 
-									</div><!-- end user-cards -->
-								<?php endif; ?>
-							<?php endforeach; ?>
-						<?php endif; ?>
+<?php
+// $deck->setCardsOnTableBackSideUrl('img/back_of_card.png');
+$back_side = $deck->getCardOnTable();
 
-					<div class="middle-row col-md-4">
-
-						<?php
-							// $deck->setCardsOnTableBackSideUrl('img/back_of_card.png');
-							$back_side = $deck->getCardOnTable();
-
-							foreach ($back_side as $key => $value):
-						?>
-							<div class="cards_on_table"><a data-id="<?php echo $value->getCardId(); ?>"><img style="z-index: <?php echo $key; ?>; right: <?php echo $key / 2; ?>px" src="<?php echo $value->href ?>" alt=""></a></div>
-						<?php endforeach; ?>
-
-					</div><!-- end middle-row -->
+foreach ($back_side as $key => $value):
+?>
+							<div class="cards_on_table"><a data-id="<?php echo $value->getCardId();?>"><img style="z-index: <?php echo $key;?>; right: <?php echo $key/2;?>px" src="<?php echo $value->href?>" alt=""></a></div>
+<?php endforeach;?>
+</div><!-- end middle-row -->
 
 					<footer></footer>
 				</div><!-- end #table -->
@@ -127,5 +124,5 @@ define('PATH_TO_CARDS_JSON_FILE', 'json/cards.json');
 </html>
 
 <?php echo '<pre>';
-print_r($deck->getUser()); ?>
+print_r($deck->getUser());?>
 
