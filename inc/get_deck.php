889e0e9a4bbaed77ea82735543bfd2d06057913c
Aydin Aydin <Aydin@Aydins-MacBook-Air.local>
2016-04-22 09:16:43 +0200

    Uppdaterade med Victorias filer

--- inc/get_deck.php
+++ inc/get_deck.php
@@ -1,8 +1,5 @@
 <?php
 require_once '../config/config.php';
-
 $test = file_get_contents('../txt/serialize_deck_obj.txt');
-$deck = unserialize($test); // unserialize deck obj from file
-
-echo json_encode($deck->getUser()); // echo all user object
-
+$deck = unserialize($test);// unserialize deck obj from file
+echo json_encode($deck->getUser());// echo all user object
